24a84c0578c2a7237489837306149ac63cecca51
Neymarz1y0 <743478304@qq.com>
2024-07-04 22:24:03 +0800

    绑定端口

--- src/main/java/com/example/mystock/config/CorsConfig.java
+++ src/main/java/com/example/mystock/config/CorsConfig.java
@@ -7,14 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8083") // Adjust this to match the port and host of your frontend application
+                       // .allowedOrigins("http://zhangliyun10.gnway.cc")
+                        .allowedOrigins("http://localhost:8083")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
